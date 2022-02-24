fcf862c04f90ff80baf0de4471d3caf699865c6b
morris-wambua <morriswambua7@gmail.com>
2022-02-24 11:52:01 +0300

    update the response body

--- src/main/java/com/morrislab/httpconcepts/controllers/UserController.java
+++ src/main/java/com/morrislab/httpconcepts/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.morrislab.httpconcepts.controllers;
 
 import com.morrislab.httpconcepts.model.User;
 import com.morrislab.httpconcepts.service.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityNotFoundException;
@@ -25,14 +27,18 @@ public class UserController {
     }
 
     @GetMapping("/users")
+    @ResponseBody
     public ResponseEntity<List<User>> showAllsers(){
-        return ResponseEntity.ok(userService.findAllUsers());
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.findAllUsers());
     }
 
     @GetMapping("user/{id}")
     public ResponseEntity<User> showSingleUser(@PathVariable(value = "id") Long id){
         Optional<User> user= Optional.of(userService.findUserById(id));
-        return ResponseEntity.ok(user
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(user
                 .orElseThrow( ()-> new EntityNotFoundException("User Not Found")));
     }
 
@@ -42,13 +48,16 @@ public class UserController {
     }
 
     @PostMapping("user/add")
-    public User addUser(@RequestBody User user){
-        return userService.saveUser(user);
+    @ResponseBody
+    public ResponseEntity<User> addUser(@RequestBody User user){
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.saveUser(user));
     }
 
     @PutMapping("/user/update/{id}")
-    public User updateUser(@RequestBody User user, @PathVariable(value = "id") Long id){
-        return userService.updateUser(user,id);
+    @ResponseBody
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable(value = "id") Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(user,id));
     }
 
 }
